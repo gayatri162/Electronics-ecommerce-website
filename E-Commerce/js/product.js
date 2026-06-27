8afcac4d515dcc2244de9360820c448d52a6c16a
Gayatri <gayatrisingh44567@gmail.com>
2026-06-27 11:19:24 +0530

    Add files via upload

---
+++ E-Commerce/js/product.js
@@ -0,0 +1,98 @@
+const products = [
+{
+    id: 1,
+    name: "Dell Laptop",
+    price: 55000,
+    image: "images/Dell Laptop.png",
+    category: "Laptop",
+    description: "High-performance, sleek laptop designed for seamless productivity."
+},
+{
+    id: 2,
+    name: "Samsung SmartPhone",
+    price: 40000,
+    image: "images/Samsung SmartPhone.png",
+    category: "Mobile",
+    description: "Samsung offers a wise range smartphone for different needs."
+},
+{
+    id: 3,
+    name: "HeadPhones",
+    price: 2000,
+    image: "images/Head phone.png",
+    category: "Audio",
+    description: "Premium over-ear headphones delivering immersive audio."
+},
+{
+    id: 4,
+    name: "Boat Earbuds",
+    price: 879,
+    image: "images/Boat Earbuds.png",
+    category: "Audio",
+    description: "Wireless earbuds with immersive sound."
+},
+{
+    id: 5,
+    name: "Fingers Pen Drive",
+    price: 499,
+    image: "images/Fingers Pen-Drive.png",
+    category: "Accessories",
+    description: "A reliable, high-speed portable flash drive for secure and easy data transfer."
+},
+{
+    id: 6,
+    name: "Wireless HP Mouse",
+    price: 2200,
+    image: "images/HP Mouse.png",
+    category: "Accessories",
+    description: "A responsive, wireless mouse engineered for smooth & precise navigation."
+},
+{
+    id: 7,
+    name: "Wireless Keyboard",
+    price: 3000,
+    image: "images/Wireless Keyboard.png",
+    category: "Accessories",
+    description: "Wireless Keyboard designed for quiet & comfortable typing."
+},
+{
+    id: 8,
+    name: "Lapcare Webcam with noise cancellation",
+    price: 5500,
+    image: "images/LAPCARE Lapcam HD .png",
+    category: "Accessories",
+    description: "efficient HD Webcam."
+},
+{
+    id: 9,
+    name: "Phone Stand Holder",
+    price: 699,
+    image: "images/Phone stand holder.png",
+    category: "Accessories",
+    description: "Adjustable phone stand for hands-free & convenient phone viewing."
+},
+{
+    id: 10,
+    name: "Bluetooth Headset",
+    price: 1100,
+    image: "images/Mic Headphone.png",
+    category: "Audio",
+    description: "A versatile headset featuring an adjustable microphone for voice communication."
+},
+{
+    id: 11,
+    name: "Smart Speaker",
+    price: 999,
+    image: "images/Smart Speaker .png",
+    category: "Audio",
+    description: "Voice-Controlled smart speaker that delivers crisp audio & effortless smart home integration."
+},
+{
+    id: 12,
+    name: "Smart Wrist Watch",
+    price: 2000,
+    image: "images/Smart Watch.png",
+    category: "Wearable",
+    description: "Latest sleek, modern smartwatch equipped with advanced fitness tracking & instant notifications"
+}
+];
