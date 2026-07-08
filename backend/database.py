b7472fd22bc713c1f55931e2541ec7c85c7680cc
gayatri <gayatri@sandeeps-MacBook-Air.local>
2026-07-08 14:31:39 +0530

    Initial commit

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,10 @@
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb://localhost:27017/")
+load_dotenv()
+
+client = MongoClient(os.getenv("MONGO_URI"))
 
 db = client["techstack"]
 
